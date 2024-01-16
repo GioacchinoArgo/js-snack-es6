c97f9040dd7daf61215c96a3b2f0deb4944216d7
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-16 22:47:15 +0100

    completed first exercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 console.log('JS OK')
 
+//# Esercizio 1
+
+const tableName = 'Tavolo Vip';
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
+
+// - 1
+const guestList = guests.map((g, i) => {
+    return {
+        tableName: 'Tavolo Vip',
+        name: g,
+        place: i++
+    }
+});
+
+console.log(guestList)
+
