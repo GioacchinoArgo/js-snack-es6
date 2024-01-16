4ebe38188710de14a416f05997d6e0569082cd69
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-17 00:31:34 +0100

    completed second exercise

--- js/script.js
+++ js/script.js
@@ -28,6 +28,63 @@ const guestList = guests.map((guest, i) => {
 
 console.log(guestList)
 
+//# Esercizio 2
+
+const students = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    },
+];
+
+//* - 1 Creo un Array con gli studenti che hanno un totale di voti superiore a 70 usando il metodo "filter".
+
+const over70StudentsGrades = students.filter(({grades}) => grades > 70);
+
+console.log('Il numero di studenti con un grado superiore al 70: ',over70StudentsGrades)
+
+//* - 2 Creo un Array con gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120 usando il metodo "filter".
+
+const over70StudentsAndIdOver120 = students.filter(({grades, id}) => grades > 70 && id > 120);
+
+console.log('Il numero di studenti con un grado superiore al 70 e id superiore a 120: ',over70StudentsAndIdOver120)
+
+//* - 3 Creo un Array con tutti i nomi degli studenti in maiuscolo tramite il metodo ".map".
+
+const studentsList = students.map(({name})  => name.toUpperCase());
+
+console.log('I nomi degli studenti sono: ',studentsList)
 
 
 
